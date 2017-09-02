5c82dcc909fbac7bbb3255cddb477e9a3b0dd785
rohanmishra97 <rohan.mishra1997@gmail.com>
2017-09-02 23:12:31 +0530

    basic impl

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ const
     port = 3000
     config = require('./config/secret')
 
+
+var complaintState = "inactive"
+var dept = "NULL"
 const app = express()
 app.use(bodyParser.json());
+
+var dummy = {
+	first_name: "Rohan",
+	last_name: "Mishra",
+	user_id: 1379630258821416,
+	phone: 9810953962,
+	email: "rohan.mishra1997@gmail.com"
+}
+
 
 app.get('/webhook',(req,res)=>{
 	if(req.query['hub.mode'] === 'subscribe' && 
@@ -22,17 +34,28 @@ app.get('/webhook',(req,res)=>{
 
 app.post('/webhook', function (req, res) {
   var data = req.body;
-  if (data.object === 'page') {
+
+  var ok = true
+  try {
+  	var val = data.entry[0].messaging[0]
+  } catch(e) {
+  	ok = false
+  }
+  if (data.object === 'page' && ok) {
     data.entry.forEach(function(entry) {
       var pageID = entry.id;
       var timeOfEvent = entry.time;
 
       entry.messaging.forEach(function(event) {
+      	if(event.postback) {
+      		console.log("Getting Started!")
+      		sendIntroText(event.sender.id)
+      	}
         if (event.message) {
         	console.log("Message received")
           receivedMessage(event);
         } else {
-          console.log("Webhook received unknown event");
+          //console.log("Webhook received unknown event");
         }
       });
     });
@@ -79,23 +102,107 @@ function sendTextMessage(receipientID, text) {
 function process(text) {
 	return (text.split("").reverse().join(""))
 }
-
+function sendIntroText(recipientID) {
+	sendTextMessage(recipientID, "Hey I'm Cody!")
+}
 function complaint(recipientID) {
-	console.log("Complaint")
-	sendTextMessage(recipientID, "Complain hogaya re babu.")
+	var messageData = {
+		'recipient': {
+			'id': recipientID
+		},
+		message: {
+			"text": "Choose Department",
+			"quick_replies": [
+				{
+					"content_type": "text",
+					"title": "Water",
+					"payload": "WATER_DIVISION"
+				},
+				{
+					"content_type": "text",
+					"title": "Electricity",
+					"payload": "ELECTRICITY_DIVISION"
+				},
+				{
+					"content_type": "text",
+					"title": "Medical",
+					"payload": "MEDICAL_DIVISION"
+				},
+				{
+					"content_type": "text",
+					"title": "Police",
+					"payload": "POLICE_DIVISION"
+				}
+			]
+		}
+	}
+	callSendAPI(messageData)
+}
+
+function getLocation(recipientID) {
+	var messageData = {
+		'recipient': {
+			'id': recipientID
+		},
+		message: {
+			"text": "Please send your location",
+			"quick_replies": [
+				{
+					"content_type": "location",
+				}
+			]
+		}
+	}
+	callSendAPI(messageData)
 }
 
 function receivedMessage(event) {
-  if(event.message.is_echo)
+	if("postback" in event) {
+		console.log("BEER")
+		sendTextMessage(event.sender.id, "Hey I'm Cody!")
+	}
+  else if(event.message.is_echo)
   {
     console.log('Received');
   }
   else if(event.message.text)
   {
-  	console.log(JSON.stringify(event.message))
-  	sendTextMessage(event.sender.id, process(event.message.text))
+  	if(complaintState === "inactive" && Object.keys(event.message.nlp).length != 0 && Object.keys(event.message.nlp.entities).length != 0 && event.message.nlp.entities.intent.length > 0) {	  	
+	  	if(event.message.nlp.entities.intent[0].value === "complaint") {
+	  		complaintState = "intentactive"
+	  		complaint(event.sender.id)
+	  	} else if(event.message.nlp.entities.intent[0].value === "sos") {
+	  		sendTextMessage(event.sender.id, "Emergency numbers : \nPolice : 5551115555\nFire : 5552225555\nAmbulance : 5553335555")
+	  	} else if(event.message.nlp.entities.intent[0].value === "info") {
+	  		info(event.sender.id)
+	  	}
+	} else {
+		if(complaintState === "intentactive") {
+			complaintState = "setlocation"
+			dept = event.message.text
+			getLocation(event.sender.id)
+		} else if(complaintState === "deptchosen") {
+			console.log(`Department: ${dept}\nComplaint: ${event.message.text}`)
+			sendTextMessage(event.sender.id, `Sending your ticket to ${dept} Division. We'll get back to you soon!`)
+			complaintState = "inactive"
+			dept = "NULL"
+		} else {
+			sendTextMessage(event.sender.id, process(event.message.text))
+			complaintState = "inactive"
+		}
+	} 
+  } else if (complaintState === "setlocation") {
+  	complaintState = "deptchosen"
+	sendTextMessage(event.sender.id, "State your complaint")
+  } else {
+  	console.log("*")
   }
 }
+
+app.post("/emergency",(req, res)=>{
+	sendTextMessage(dummy.user_id, "Cyclone in Khandagiri")
+	res.status(200).send("Sent")
+})
 
 
 
