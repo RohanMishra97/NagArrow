57ecfc36082867128e485a661429350776ee2247
rohanmishra97 <rohan.mishra1997@gmail.com>
2017-09-03 12:29:46 +0530

    Finale

--- server.js
+++ server.js
@@ -6,12 +6,26 @@ bodyParser = require('body-parser')
 path = require('path')
 port = 3000
 config = require('./config/secret')
+fs = require('fs')
 
+const app = express()
 
 var complaintState = "inactive"
+var translateState = "inactive"
+var fromLan = "English"
+var toLan = "English"
+var text = "How are you?"
 var dept = "NULL"
-const app = express()
+var tips = ['Make the most of our modern Metro. You will be hardpressed to find a destination whose journey won\'t be aided through it.',
+			'Be careful of what you eat. While street food can be enticing and delicous, you wouldn\'t want to catch a case of the Delhi Belly',
+			'Dress appropriately. Some of our main attractions have religuous foundations. Kindly respect such establishments',
+			'At night, prefer travelling in groups rather than solo. Always be cognizant of your surroundings']
 var complaints = []
+var ticketid = 1
+
+fs.readFile('ledger.json', function(err,data) {
+	complaints = JSON.parse(data)
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -105,8 +119,12 @@ function process(text) {
 	return (text.split("").reverse().join(""))
 }
 function sendIntroText(recipientID) {
-	sendTextMessage(recipientID, "Hey I'm Cody!")
+	var introText = "Hey! I'm Cody\n I'm here to help you get about the city.\n What can I do? Glad you asked!"
+	var features = "Ask for places to visit, make a complaint, get notified of events happening around you and much more! Bow wow!"
+	sendTextMessage(recipientID, introText)
+	sendTextMessage(recipientID, features)
 }
+
 function complaint(recipientID) {
 	var messageData = {
 		'recipient': {
@@ -141,18 +159,163 @@ function complaint(recipientID) {
 	callSendAPI(messageData)
 }
 
+function giveTips(recipientID) {
+	var use = tips.slice(0, 2)
+	tips = tips.slice(2, 4)
+	tips = tips.concat(use)
+	sendTextMessage(recipientID, use[0])
+	sendTextMessage(recipientID, use[1])
+}
+
 function getLocation(recipientID) {
 	var messageData = {
 		'recipient': {
 			'id': recipientID
 		},
-		message: {
+		"message": {
 			"text": "Please send your location",
 			"quick_replies": [
 			{
 				"content_type": "location",
 			}
 			]
+		}
+	}
+	callSendAPI(messageData)
+}
+
+function decode(s) {
+	return decodeURIComponent(escape(s))
+}
+function Translate(recipientID){
+
+
+
+	var sym={
+		'English':'en',
+		'Hindi':'hi',
+		'Russian':'ru',
+		'Spanish':'es',
+		'French':'fr',
+	
+	}
+	var messageData = {
+		'recipient': {
+			'id': recipientID
+		},
+		"message": {
+			"text": "Some error occurred ?",
+		}
+	}
+
+	
+	var url='https://translate.yandex.net/api/v1.5/tr.json/translate?'
+	var KEY='trnsl.1.1.20170902T200819Z.cb2c5c381d54ef25.22e2e62b652fd891761b819e8aefcfdaf99374b5'
+	var format='plain'
+	var lang=sym[fromLan]+'-'+sym[toLan];
+	var param={'lang':lang,'key':KEY,'text':text,'format':format}
+	var request = require('request');
+	var headers = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded'
+	}
+
+	var options = {
+    url: url,
+    method: 'POST',
+    headers: headers,
+    qs: param,
+}
+	var ret='Some error occurred.Sorry !'
+	var done=false
+	request(options, function (error, response, body) {
+	console.log(body)
+    if (!error && response.statusCode == 200) {
+    	dic=JSON.parse(body)
+    	text=dic['text'][0]
+    	//text=decode_utf8(text)
+    	console.log('yeh hai translated'+text)
+    	//ret=decode(text)
+    	ret = text
+    	console.log(typeof(text))
+    	messageData["message"]['text']=ret
+    	callSendAPI(messageData)
+
+    }
+    else
+    {
+    	callSendAPI(messageData)
+    }
+})	
+}
+
+function DelhiInfo(recipientID, more = 0) {
+	var links = ["Check these out", "What do you wanna know?", 
+	"How about these?", ":)", "Hope this helps!"];
+	if(more === 1) {
+		links = ["Still curious?", "Sure!", "Glad to help"];
+	}
+	var randIdx = Math.random() * links.length;
+	randIdx = parseInt(randIdx, 10);
+	var link = links[randIdx];
+	
+	var messageData = {
+		'recipient': {
+			'id': recipientID
+		},
+		message: {
+			"text": link,
+			"quick_replies": [
+			{
+				"content_type": "text",
+				"title": "Tips please",
+				"payload": "WATER_DIVISION"
+			},
+			{
+				"content_type": "text",
+				"title": "Places to visit",
+				"payload": "ELECTRICITY_DIVISION"
+			},
+			{
+				"content_type": "text",
+				"title": "Fun fact",
+				"payload": "MEDICAL_DIVISION"
+			},
+			{
+				"content_type": "text",
+				"title": "Places to eat",
+				"payload": "MEDICAL_DIVISION"
+			},
+			{
+				"content_type": "text",
+				"title": "Weather",
+				"payload": "POLICE_DIVISION"
+			}
+			]
+		}
+	}
+	callSendAPI(messageData)
+}
+
+function haveFun(recipientID) {
+	var links = ["https://imgur.com/2YahJvd", "https://imgur.com/0DIWcuN", 
+	"https://imgur.com/2DPrBJq", "https://imgur.com/azjB8WC", "https://imgur.com/YsgDp8n", "https://imgur.com/gqDrgFM",
+	"https://imgur.com/LmdOHsl⁠⁠⁠⁠"];
+	var randIdx = Math.random() * links.length;
+	randIdx = parseInt(randIdx, 10);
+	var link = links[randIdx] + '.png';
+	console.log(link)
+	var messageData = {
+		"recipient":{
+			"id": recipientID
+		},
+		"message":{
+			"attachment":{
+				"type":"image",
+				"payload":{
+					"url":link
+				}
+			}
 		}
 	}
 	callSendAPI(messageData)
@@ -269,46 +432,74 @@ function receivedMessage(event) {
 	}
 	else if(event.message.text)
 	{
+
 		if(complaintState === "inactive" && Object.keys(event.message.nlp).length != 0 && Object.keys(event.message.nlp.entities).length != 0 && event.message.nlp.entities.intent.length > 0) {	  	
 			if(event.message.nlp.entities.intent[0].value === "complaint") {
 				complaintState = "intentactive"
 				complaint(event.sender.id)
+			} else if(event.message.nlp.entities.intent[0].value === "tips") {
+				giveTips(event.sender.id)
 			} else if(event.message.nlp.entities.intent[0].value === "sos") {
 				sendTextMessage(event.sender.id, "Emergency numbers : \nPolice : 5551115555\nFire : 5552225555\nAmbulance : 5553335555")
 			} else if(event.message.nlp.entities.intent[0].value === "visit") {
 				sendTextMessage(event.sender.id, "Here are a few tourist attractions")
 				placesToVisit(event.sender.id)
-			} else if(event.message.nlp.entities.intent[0].value === "info") {
-				console.log("jere")
-				info(event.sender.id)
-			}
+			} else if(event.message.nlp.entities.intent[0].value === "Info") {
+				DelhiInfo(event.sender.id)
+			} else if(event.message.nlp.entities.intent[0].value === "fun") {
+				haveFun(event.sender.id)
+			} else if(event.message.nlp.entities.intent[0].value === "curious") {
+				DelhiInfo(event.sender.id, 1)
+			} else if(event.message.nlp.entities.intent[0].value === "translate") {
+				translateState = "gotfrom"
+				sendTextMessage(event.sender.id, "What language do you want to translate from?")
+			} 
 		} else {
 			if(complaintState === "intentactive") {
 				complaintState = "setlocation"
 				dept = event.message.text
 				getLocation(event.sender.id)
 			} else if(complaintState === "deptchosen") {
+				console.log("here")
 				var obj = {
 					user_id: event.sender.id,
 					department: dept,
-					complaint: event.message.text
+					complaint: event.message.text,
+					ticket_id: ticketid
 				}
-				complaints.push(obj)
-				console.log(complaints)
-				//console.log(`Department: ${dept}\nComplaint: ${event.message.text}`)
+				ticketid = ticketid + 1
+				fs.readFile('ledger.json',function(err,data){
+					complaints.push(obj);
+					fs.writeFile('ledger.json',JSON.stringify(complaints))
+				})	
 				sendTextMessage(event.sender.id, `Sending your ticket to ${dept} Division. We'll get back to you soon!`)
 				complaintState = "inactive"
 				dept = "NULL"
+			
+			} else if(translateState === "gotfrom"){
+				fromLan = event.message.text
+				translateState = "gotto"
+				sendTextMessage(event.sender.id,"What language do you want to translate to?")
+			} else if(translateState === "gotto"){
+				toLan = event.message.text
+				translateState = "gottext"
+				sendTextMessage(event.sender.id,"Say something..")
+			} else if(translateState === "gottext") {
+				text = event.message.text
+				translateState = "inactive"
+				Translate(event.sender.id)
 			} else {
-				sendTextMessage(event.sender.id, process(event.message.text))
+				sendTextMessage(event.sender.id, "Pardon")
 				complaintState = "inactive"
+				translateState = "inactive"
 			}
 		} 
 	} else if (complaintState === "setlocation") {
 		complaintState = "deptchosen"
-		sendTextMessage(event.sender.id, "State your complaint")
+		sendTextMessage(event.sender.id, `Write your complaint for the ${dept} Division and you'll be contacted`)
 	} else {
 		console.log("*")
+		sendTextMessage(event.sender.id, "Pardon?")
 	}
 }
 
@@ -319,7 +510,7 @@ app.post("/emergency",(req, res)=>{
 	var password = req.body.password
 	if(password === "password") {
 		sendTextMessage(dummy.user_id, `${department} issued the following announcement -\n${message}`)
-		res.status(200).redirect("/")
+		res.status(200).redirect("/login")
 	} else {
 		res.redirect("/emergency")
 	}
@@ -334,10 +525,27 @@ app.get("/login",(req, res)=>{
 })
 app.post("/login",(req,res)=>{
 	if(req.body.password === "password") {
-		res.redirect("/profile/"+req.body.username)
+		res.redirect("/profile/"+req.body.name)
 	} else {
 		res.redirect("/login")
 	}
+})
+
+app.get("/profile/:name",(req,res)=>{
+	var name = req.params.name
+	name = name.split("%20").join(" ")
+	var deptComplaint = []
+	complaints.forEach(complaint => {
+		if(complaint.department === name) {
+			deptComplaint.push(complaint)
+		}
+	})
+	var obj = {
+		department: name,
+		complaints: deptComplaint
+	}
+	console.log(JSON.stringify(obj))
+	res.render("profile",{data: obj})
 })
 
 app.get('/',(req,res)=>{
