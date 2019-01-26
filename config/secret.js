7be6d97dc77f19c7ff66e0cff2fe7499776ac033
Rohan Mishra <rohan.mishra1997@gmail.com>
2019-01-26 14:18:56 +0530

    Delete secret.js

--- config/secret.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	'page_access_token' : 'EAAENlerQG1YBAJ5QAQNAij45qsVMFOsBF1fyZCQHgtS44hyFgnBICn88PyH2fagPgC4dHXASdPgpyQelPFPgIZCFfFh18O9sId9vrOBxfgKqkoEEpYEjaf9XkDT0PgPlKLhumMjZAoiwUnF0xRde2x3QUWQbIQYXQCBLZAkRfwZDZD',
-	'app_id':'296412517505878',
-	'app_secret':'e070e5ae343d0ae9ba3d300509564252',
-	'verify_token':'placeholders'
-}
